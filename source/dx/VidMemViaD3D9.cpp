0672338abb3930e9337f9e7f791b8d77df71aed7
aziznefzi <nefziaziz763gmail.com>
2026-02-21 12:41:09 +0100

    Fix ENB Series / DirectX 9 crashes caused by device leak, setup Github Action.

--- source/dx/VidMemViaD3D9.cpp
+++ source/dx/VidMemViaD3D9.cpp
@@ -27,58 +27,14 @@
 #endif
 
 HRESULT GetVideoMemoryViaD3D9(HMONITOR hMonitor, UINT* pdwAvailableTextureMem) {
-    bool hr = false;
-    bool bGotMemory = false;
     *pdwAvailableTextureMem = 0;
 
-    IDirect3D9* pD3D9 = nullptr;
-    pD3D9 = Direct3DCreate9(D3D_SDK_VERSION);
-    if (pD3D9) {
-        UINT dwAdapterCount = pD3D9->GetAdapterCount();
-        for (UINT iAdapter = 0; iAdapter < dwAdapterCount; iAdapter++) {
-            IDirect3DDevice9* pd3dDevice = nullptr;
-
-            HMONITOR hAdapterMonitor = pD3D9->GetAdapterMonitor(iAdapter);
-            if (hMonitor != hAdapterMonitor)
-                continue;
-
-            HWND hWnd = GetDesktopWindow();
-
-            D3DPRESENT_PARAMETERS pp = {};
-            pp.BackBufferWidth = 800;
-            pp.BackBufferHeight = 600;
-            pp.BackBufferFormat = D3DFMT_R5G6B5;
-            pp.BackBufferCount = 1;
-            pp.MultiSampleType = D3DMULTISAMPLE_NONE;
-            pp.MultiSampleQuality = 0;
-            pp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-            pp.hDeviceWindow = hWnd;
-            pp.Windowed = TRUE;
-
-            pp.EnableAutoDepthStencil = FALSE;
-            pp.Flags = 0;
-            pp.FullScreen_RefreshRateInHz = 0;
-            pp.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;
-
-            if (pd3dDevice = GetD3DDevice()) {
-                hr = true;
-                //pD3D9->CreateDevice(iAdapter, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &pp, &pd3dDevice);
-            }
-
-            if (hr) {
-                *pdwAvailableTextureMem = pd3dDevice->GetAvailableTextureMem();
-                bGotMemory = true;
-                SAFE_RELEASE(pd3dDevice);
-            }
-        }
-
-        SAFE_RELEASE(pD3D9);
+    if (IDirect3DDevice9* pd3dDevice = GetD3DDevice()) {
+        *pdwAvailableTextureMem = pd3dDevice->GetAvailableTextureMem();
+        return S_OK;
     }
 
-    if (bGotMemory)
-        return S_OK;
-    else
-        return E_FAIL;
+    return E_FAIL;
 }
 
 
